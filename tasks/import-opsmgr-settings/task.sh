e335ef2c36ff989edf87f5fbd071ac332574f049
caresourceplatform <david.peyton@caresource.com>
2019-05-16 14:51:29 -0400

    Trying without the quotes around the decryption passphrase

--- tasks/import-opsmgr-settings/task.sh
+++ tasks/import-opsmgr-settings/task.sh
@@ -28,12 +28,13 @@ function main() {
   printf '\n'
 
   om-linux --target "https://${OPSMAN_DOMAIN_OR_IP_ADDRESS}" \
+      --decryption-passphrase $OPSMAN_PASSPHRASE \
       --skip-ssl-validation \
       --request-timeout 86400 \
       import-installation \
       --installation "${cwd}/opsmgr-settings/${OPSMAN_SETTINGS_FILENAME}"
  }
 
- export OM_DECRYPTION_PASSPHRASE="${OPSMAN_PASSPHRASE}"
+ export OM_DECRYPTION_PASSPHRASE=$OPSMAN_PASSPHRASE
 
  main "${PWD}"
